57f5e0e787404f0f13b5b2133c080b044c6ca768
Jgarette0 <aajiliangarette@gmail.com>
2023-11-24 00:09:18 +0800

    hmm

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,39 +1,5 @@
-import { useState } from "react";
-
 export function Food() {
-  let FavouriteFood = [
-    "Fried Chicken",
-    "Mango Float",
-    "Cheese Cake",
-    "Crab Meat",
-    "Sizzling Pork",
-  ];
-
-  const [selectedIndex, setSelectedIndex] = useState(-1);
-  return (
-    <>
-      <h1>Favourite Foods</h1>
-      <ul className="list-group">
-        {FavouriteFood.length === 0 && <p>No Favourite Food found..</p>}
-        {FavouriteFood.map((FavouriteFood, index) => (
-          <li
-            key={FavouriteFood}
-            className={
-              selectedIndex === index
-                ? "list-group-item active autofocus"
-                : "list-group-item"
-            }
-            onClick={() => {
-              setSelectedIndex(index);
-              console.log(FavouriteFood, index + 1);
-            }}
-          >
-            {FavouriteFood}
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+  return <h1>Hello World again</h1>;
 }
 
 export default Food;
